c1b3faf3c2cc1188dcfd3e2eb9afea8b559d0d3a
Matteo Ronchi <matteo.ronchi@gmail.com>
2015-08-24 17:21:52 +0200

    Update style coding for demo app main container

--- examples/simple/components/App.js
+++ examples/simple/components/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import { createStore, combineReducers, compose } from 'redux';
-import { Provider } from 'react-redux';
+import {createStore, combineReducers, compose} from 'redux';
+import {Provider} from 'react-redux';
 
-import { reduxComponentStateStore } from 'redux-component-state';
+import {reduxComponentStateStore} from 'redux-component-state';
 
 import appInfo from '../reducers/appInfo';
-
 import CounterPanel from './CounterPanel';
 
 const combinedReducers = combineReducers({ appInfo });
-
-let createFinalStore = compose(
-    reduxComponentStateStore,
-    createStore
-    );
-
+const createFinalStore = compose( reduxComponentStateStore, createStore);
 const store = createFinalStore(combinedReducers);
 
-
 export default class App extends Component {
-
   render() {
     return (
       <Provider store={store}>

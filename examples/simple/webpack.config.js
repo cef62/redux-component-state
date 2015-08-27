42c90a58e94db2e5a05764ffdf34eec162aeca34
Matteo Ronchi <matteo.ronchi@gmail.com>
2015-08-27 17:30:09 +0200

    Updated example to use port 3001 instead of 3000

--- examples/simple/webpack.config.js
+++ examples/simple/webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'source-maps',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
+    'webpack-dev-server/client?http://localhost:3001',
     'webpack/hot/only-dev-server',
     './index'
   ],

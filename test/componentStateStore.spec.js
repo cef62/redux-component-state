d90a0c9833d3311c603a0253e106220a14e19c62
Matteo Ronchi <matteo.ronchi@gmail.com>
2015-08-30 22:26:51 +0200

    Removed mocha and chai and added tape.js. Updated tests to use tape.

--- test/componentStateStore.spec.js
+++ test/componentStateStore.spec.js
@@ -1,176 +1,198 @@
-import { expect } from 'chai';
-// import { expect, use } from 'chai';
-// import sinon from 'sinon';
-// import sinonChai from 'sinon-chai';
+import test from 'tape';
+
 import { createStore, combineReducers, compose } from 'redux';
 import { reduxComponentStateStore } from '../src';
 import { addTodo } from './helpers/actionCreators';
 import * as reducers from './helpers/reducers';
 
-// use(sinonChai);
+const space = '  ';
+const setup = () => {
+  // create redux store
+  const createFinalStore = compose( reduxComponentStateStore, createStore);
+  const store = createFinalStore(combineReducers(reducers));
 
-describe('reduxComponentStateStore', () => {
-  let createFinalStore;
-  let store;
+  // create redux reducer for component state
+  const reducer = combineReducers({ todos: reducers.todos });
 
-  beforeEach('create redux store', () => {
-    createFinalStore = compose( reduxComponentStateStore, createStore);
-    store = createFinalStore(combineReducers(reducers));
+  // setup subscription API test variables
+  const subscribe = store.componentState.subscribe;
+  const minimalConfig = {
+    key: '',
+    reducer,
+  };
+  const completeConfig = Object.assign({}, minimalConfig, {
+    initialState: {
+      todos: [{
+        id: 999,
+        text: 're-hydratated todo',
+      }],
+    },
   });
+  return {store, reducer, subscribe, minimalConfig, completeConfig};
+};
 
-  it('should expose the public API', () => {
-    const methods = Object.keys(store);
 
-    expect(methods).to.have.length(6);
-    expect(store).to.include.keys('subscribe');
-    expect(store).to.include.keys('dispatch');
-    expect(store).to.include.keys('getState');
-    expect(store).to.include.keys('getReducer');
-    expect(store).to.include.keys('replaceReducer');
-    expect(store).to.include.keys('componentState');
-    expect(store.componentState).to.include.keys('subscribe');
-  });
+test('reduxComponentStateStore', ({end}) => end() );
 
-  describe('componentState.subscribe()', () => {
-    let subscribe;
-    let minimalConfig;
-    let completeConfig;
-    let reducer;
+test(`.${space}should be a function`, ({ok, equal, end}) => {
+  ok(reduxComponentStateStore, 'should exists');
+  equal(typeof reduxComponentStateStore, 'function');
+  end();
+});
 
-    beforeEach('create redux reducer for component state', () => {
-      reducer = combineReducers({ todos: reducers.todos });
-    });
+test(`.${space}should expose the public API`, ({equal, end}) => {
+  const { store } = setup();
+  const methods = Object.keys(store);
 
-    beforeEach('setup subscription API test variables', () => {
-      subscribe = store.componentState.subscribe;
-      minimalConfig = {
-        key: '',
-        reducer,
-      };
-      completeConfig = Object.assign({}, minimalConfig, {
-        initialState: {
-          todos: [{
-            id: 999,
-            text: 're-hydratated todo',
-          }],
-        },
-      });
-    });
+  equal(methods.length, 6);
+  ['subscribe', 'dispatch', 'getState', 'getReducer', 'replaceReducer']
+    .forEach( key => equal(typeof store[key], 'function', `API missing: ${key}`) );
 
-    it('should require a valid configuration object', () => {
-      expect( () => subscribe() ).to.throw(Error);
-      expect( () => subscribe({}) ).to.throw(Error);
-      expect( () => subscribe({a: 33, b: 'temp'}) ).to.throw(Error);
-      minimalConfig.key = 'keyA';
-      expect( () => subscribe(minimalConfig) ).to.not.throw(Error);
-      expect( () => subscribe(minimalConfig) ).to.throw(Error);
-      completeConfig.key = 'keyB';
-      expect( () => subscribe(completeConfig) ).to.not.throw(Error);
-      Reflect.deleteProperty(completeConfig.initialState, 'todos');
-      expect( () => subscribe(completeConfig) ).to.throw(Error);
-    });
+  equal(typeof store.componentState, 'object', `API missing: componentState`);
+  equal(typeof store.componentState.subscribe, 'function', `API missing: componentState.subscribe`);
 
-    it('should return a subscription object', () => {
-      minimalConfig.key = 'keyA';
-      const subscription = subscribe(minimalConfig);
+  end();
+});
 
-      expect( subscription ).to.exist;
-      expect( subscription ).to.be.an('object');
-    });
+test(`.${space}should require a valid configuration object`, ({throws, doesNotThrow, end}) => {
+  const { subscribe, minimalConfig, completeConfig } = setup();
+  throws( subscribe );
+  throws( () => subscribe({}) );
+  throws( () => subscribe({a: 33, b: 'temp'}) );
 
-    it('should return a subscription object, and it should expose the public API', () => {
-      minimalConfig.key = 'keyA';
-      const subscription = subscribe(minimalConfig);
+  minimalConfig.key = 'keyA';
+  doesNotThrow( () => subscribe(minimalConfig) );
+  throws( () => subscribe(minimalConfig) );
 
-      const api = Object.keys(subscription);
+  completeConfig.key = 'keyB';
+  doesNotThrow( () => subscribe(completeConfig) );
 
-      expect( api ).to.have.length(4);
-      expect( subscription ).to.include.keys('unsubscribe');
-      expect( subscription ).to.include.keys('dispatch');
-      expect( subscription ).to.include.keys('getState');
-      expect( subscription ).to.include.keys('storeKey');
+  Reflect.deleteProperty(completeConfig.initialState, 'todos');
+  throws( () => subscribe(completeConfig) );
 
-      expect( subscription.unsubscribe ).to.be.a('function');
-      expect( subscription.dispatch ).to.be.a('function');
-      expect( subscription.getState ).to.be.a('function');
-      expect( subscription.storeKey ).to.be.a('string');
-    });
+  end();
+});
 
-    it('should create a new store with name ending with the given subscription key', () => {
-      const key = 'keyA';
-      minimalConfig.key = key;
-      const subscription = subscribe(minimalConfig);
+test(`.${space}componentState.subscribe()`, ({end}) => end() );
 
-      const match = Object.keys(store.getState()).find( k => k.endsWith(key) );
-      expect( match ).to.exist;
+test(`.${space}${space}should return a subscription object`, ({ok, equal, end}) => {
+  const { subscribe, minimalConfig } = setup();
+  minimalConfig.key = 'keyA';
+  const subscription = subscribe(minimalConfig);
 
-      const state = store.getState()[subscription.storeKey];
-      expect( state ).to.exist;
-      expect( state.todos ).to.be.an('array');
-    });
+  ok( subscription );
+  equal( typeof subscription, 'object' );
 
-    it('should create a new store populated with given initial state', () => {
-      completeConfig.key = 'keyA';
-      const subscription = subscribe(completeConfig);
+  end();
+});
 
-      const todos = subscription.getState().todos;
-      expect( todos ).to.have.length(1);
-      expect( todos[0].id ).to.equal(completeConfig.initialState.todos[0].id);
-      expect( todos[0].text ).to.equal(completeConfig.initialState.todos[0].text);
-    });
+test(`.${space}${space}returned object should expose the public API`, ({ok, equal, end}) => {
+  const { subscribe, minimalConfig } = setup();
+  minimalConfig.key = 'keyA';
+  const subscription = subscribe(minimalConfig);
 
-    describe('.unsubscribe()', () => {
-      it('should remove the component store from redux after unsubscribe is invoked', () => {
-        const key = 'keyA';
-        minimalConfig.key = key;
-        const subscription = subscribe(minimalConfig);
-        subscription.unsubscribe();
-        const match = Object.keys(store.getState()).find( k => k.endsWith(key) );
-        expect( match ).to.not.exist;
-      });
-    });
+  const api = Object.keys(subscription);
+  equal( api.length, 4 );
 
-    describe('.dispatch()', () => {
-      it('should update the component specific state', () => {
-        const key = 'keyA';
-        minimalConfig.key = key;
-        const subscription = subscribe(minimalConfig);
-        subscription.dispatch(addTodo('new todo'));
+  ok( subscription.unsubscribe );
+  ok( subscription.dispatch );
+  ok( subscription.getState );
+  ok( subscription.storeKey );
 
-        let todos = subscription.getState().todos;
-        expect( todos ).to.have.length(1);
-        expect( todos[0].text ).to.equal('new todo');
+  equal( typeof subscription.unsubscribe, 'function' );
+  equal( typeof subscription.dispatch, 'function' );
+  equal( typeof subscription.getState, 'function' );
+  equal( typeof subscription.storeKey, 'string' );
 
-        todos = store.getState()[subscription.storeKey].todos;
-        expect( todos ).to.have.length(1);
-        expect( todos[0].text ).to.equal('new todo');
-      });
+  end();
+});
 
-      it('should throw if invoked after unsubscription', () => {
-        const key = 'keyA';
-        minimalConfig.key = key;
-        const subscription = subscribe(minimalConfig);
-        const action = () => subscription.dispatch(addTodo('new todo'));
-        action();
+test(`.${space}${space}should create a new store with name ending with the given subscription key`, ({ok, end}) => {
+  const { store, subscribe, minimalConfig } = setup();
+  const key = 'keyA';
+  minimalConfig.key = key;
+  const subscription = subscribe(minimalConfig);
 
-        subscription.unsubscribe();
-        expect( action ).to.throw(Error);
-      });
-    });
+  const match = Object.keys(store.getState()).find( k => k.endsWith(key) );
+  ok( match );
 
-    describe('.getState()', () => {
-      it('should throw if invoked after unsubscription', () => {
-        const key = 'keyA';
-        minimalConfig.key = key;
-        const subscription = subscribe(minimalConfig);
+  const state = store.getState()[subscription.storeKey];
+  ok( Array.isArray(state.todos) );
 
-        const state = subscription.getState();
-        expect( state ).to.exist;
-        expect( state.todos ).to.be.an('array');
+  end();
+});
 
-        subscription.unsubscribe();
-        expect( subscription.getState ).to.throw(Error);
-      });
-    });
-  });
+test(`.${space}${space}should create a new store populated with given initial state`, ({equal, end}) => {
+  const { subscribe, completeConfig } = setup();
+  completeConfig.key = 'keyA';
+  const subscription = subscribe(completeConfig);
+
+  const todos = subscription.getState().todos;
+  equal( todos.length, 1 );
+  equal( todos[0].id, completeConfig.initialState.todos[0].id );
+  equal( todos[0].text, completeConfig.initialState.todos[0].text );
+
+  end();
+});
+
+test(`.${space}componentState.unsubscribe()`, ({end}) => end() );
+
+test(`.${space}${space}should remove the component store from redux`, ({notOk, end}) => {
+  const { store, subscribe, minimalConfig } = setup();
+  const key = 'keyA';
+  minimalConfig.key = key;
+  const subscription = subscribe(minimalConfig);
+  subscription.unsubscribe();
+  const match = Object.keys(store.getState()).find( k => k.endsWith(key) );
+  notOk( match );
+  end();
+});
+
+
+test(`.${space}componentState.dispatch()`, ({end}) => end() );
+
+test(`.${space}${space}should update the state assigned to the component`, ({equal, end}) => {
+  const { store, subscribe, minimalConfig } = setup();
+  minimalConfig.key = 'keyA';
+  const subscription = subscribe(minimalConfig);
+  subscription.dispatch(addTodo('new todo'));
+
+  let todos = subscription.getState().todos;
+  equal( todos.length, 1 );
+  equal( todos[0].text, 'new todo' );
+
+  todos = store.getState()[subscription.storeKey].todos;
+  equal( todos.length, 1 );
+  equal( todos[0].text, 'new todo' );
+
+  end();
+});
+
+test(`.${space}${space}should throw if invoked after unsubscription`, ({throws, end}) => {
+  const { subscribe, minimalConfig } = setup();
+  minimalConfig.key = 'keyA';
+  const subscription = subscribe(minimalConfig);
+  const action = () => subscription.dispatch(addTodo('new todo'));
+  action();
+  subscription.unsubscribe();
+  throws( action );
+
+  end();
+});
+
+test(`.${space}componentState.getState()`, ({end}) => end() );
+
+test(`.${space}${space}should throw if invoked after unsubscription`, ({throws, ok, end}) => {
+  const { subscribe, minimalConfig } = setup();
+  minimalConfig.key = 'keyA';
+  const subscription = subscribe(minimalConfig);
+
+  const state = subscription.getState();
+  ok( state );
+  ok( Array.isArray(state.todos) );
+
+  subscription.unsubscribe();
+  throws( subscription.getState );
+
+  end();
 });

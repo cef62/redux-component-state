b8331304899d56bb7355c0a9fef9b05eb3065e4a
Matteo Ronchi <matteo.ronchi@gmail.com>
2015-09-04 23:31:34 +0200

    Added better error management for global middlewares of comonent state reducers

--- src/componentStateStore.js
+++ src/componentStateStore.js
@@ -9,187 +9,201 @@ import {
 import { compose } from 'redux';
 import validateSubscription from './utils/validateSubscription';
 
-export default function createComponentStateStore(next) {
-  // map of component state subscribers
-  const subscribersMap = {};
-
-  // redux store
-  let store;
-
-  // ****************************************************************
-  // getState method,
-  // the state is sliced with only the specific component state of
-  // the subscriber
-  // ****************************************************************
-
-  function getStateKey(key) {
-    return KEY + key;
+export default function createComponentStateStore(...middlewares) {
+  if ( middlewares.some(middleware => typeof middleware !== 'function') ) {
+    throw new Error('Redux Component State Store accept only function as parameters');
   }
 
-  function getState(key) {
-    if (!subscribersMap[key]) {
-      throw new Error(`redux-component-state, trying to retrieve state for an unknown subscriber: ${key}`);
-    }
-    return store.getState()[key];
-  }
-  // ****************************************************************
-  // dispatch method for component state actions
-  // ****************************************************************
+  return (next) => {
+    // map of component state subscribers
+    const subscribersMap = {};
 
-  function dispatch(key, action) {
-    if (!subscribersMap[key]) {
-      throw new Error(`redux-component-state, trying to dispatch actions for an unknown subscriber: ${key}`);
+    // redux store
+    let store;
+
+    // ****************************************************************
+    // getState method,
+    // the state is sliced with only the specific component state of
+    // the subscriber
+    // ****************************************************************
+
+    function getStateKey(key) {
+      return KEY + key;
     }
 
-    return store.dispatch({
-      type: STATE_ACTION,
-      subType: ACTION,
-      key: key,
-      data: action,
-    });
-  }
+    function getState(key) {
+      if (!subscribersMap[key]) {
+        throw new Error(`redux-component-state, trying to retrieve state for an unknown subscriber: ${key}`);
+      }
+      return store.getState()[key];
+    }
+    // ****************************************************************
+    // dispatch method for component state actions
+    // ****************************************************************
 
-  // ****************************************************************
-  // unsubscribe method,
-  // passed upon the subscription
-  // ****************************************************************
+    function dispatch(key, action) {
+      if (!subscribersMap[key]) {
+        throw new Error(`redux-component-state, trying to dispatch actions for an unknown subscriber: ${key}`);
+      }
 
-  function unsubscribe(key) {
-    // unmount the component store
-    store.dispatch({ type: STATE_ACTION, subType: UNMOUNT, key });
-
-    // clear subscriber data
-    Reflect.deleteProperty(subscribersMap, key);
-  }
-
-  // ****************************************************************
-  // subscribe method,
-  // passed to children via `store context`
-  // ****************************************************************
-
-  function subscribe(subscription) {
-    validateSubscription(subscription);
-
-    const { key, reducer, initialState, middlewares } = subscription;
-
-    // compose unique store-key
-    const storeKey = getStateKey(key);
-
-    if (subscribersMap[storeKey]) {
-      throw new Error(`The redux componentStore with key: ${key} is already registered!`);
+      return store.dispatch({
+        type: STATE_ACTION,
+        subType: ACTION,
+        key: key,
+        data: action,
+      });
     }
 
-    // create redux reducer function
-    subscribersMap[storeKey] = reducer;
+    // ****************************************************************
+    // unsubscribe method,
+    // passed upon the subscription
+    // ****************************************************************
 
-    const middlewareAPI = {
-      getState: getState.bind(null, key),
-      dispatch: (action) => dispatch(action),
-    };
-    const chain = middlewares.map(middleware => middleware(middlewareAPI));
-    const componentStateDispatch = dispatch.bind(null, storeKey);
-    const enhancedDispatch = compose(...chain)(componentStateDispatch);
+    function unsubscribe(key) {
+      // unmount the component store
+      store.dispatch({ type: STATE_ACTION, subType: UNMOUNT, key });
 
-    // mount the new state on the redux store
-    store.dispatch({
-      type: STATE_ACTION,
-      subType: MOUNT,
-      key: storeKey,
-      state: initialState,
-    });
-
-    // return unsuscriber function
-    return {
-      storeKey,
-      dispatch: enhancedDispatch,
-      unsubscribe: unsubscribe.bind(null, storeKey),
-      getState: getState.bind(null, storeKey),
-    };
-  }
-
-  // ****************************************************************
-  // Reducer wrapper function,
-  // use internally to create a component state reducer
-  // ****************************************************************
-
-  /*
-   * Component State Action Signature:
-   * {
-   *  key: {required} {string},
-   *  type: {required} {string},
-   *  subType: {required} {string},
-   *  state: {optional} {object} pre-composed state to map to the component
-   *          state, used to override initial component state mounting default
-   *          value
-   *  data: {required for subtype === ACTION } original action submitted
-   * }
-   */
-  const reactions = {
-    [MOUNT]: (state, action, reducer) => {
-      state[action.key] = action.state || reducer(undefined, { type: INIT });
-      return state;
-    },
-
-    [UNMOUNT]: (state, action) => {
-      delete state[action.key];
-      return state;
-    },
-
-    [ACTION]: (state, action, reducer) => {
-      state[action.key] = reducer(state[action.key], action.data);
-      return state;
-    },
-  };
-
-  function applyComponentStateReducers(action, state, newState) {
-    const { key, type, subType } = action;
-    const reducer = subscribersMap[key];
-
-    const tmpState = Object.keys(subscribersMap).reduce( (acc, storeKey) => {
-      acc[storeKey] = (state || {})[storeKey];
-      return acc;
-    }, {});
-
-    // test if the action received is bound to a component state
-    if (type === STATE_ACTION && reducer) {
-      tmpState[key] = tmpState[key] || {};
-
-      // react properly to component state actions
-      const reaction = reactions[subType] || ( (cs) => cs );
-      reaction(tmpState, action, reducer);
+      // clear subscriber data
+      Reflect.deleteProperty(subscribersMap, key);
     }
-    Object.assign(newState, tmpState);
-    return newState;
-  }
 
-  function componentStateReducer(reducer) {
-    // `reducer` is the received original redux store reducer function
-    // return reducer method signature
-    return (state, action) => {
-      // process action with the original reducer
-      let newState = reducer(state, action);
-      newState = applyComponentStateReducers(action, state, newState);
-      return newState;
-    };
-  }
+    // ****************************************************************
+    // subscribe method,
+    // passed to children via `store context`
+    // ****************************************************************
 
-  // ****************************************************************
-  // returned Higher Order Store Creator function
-  // ****************************************************************
+    function subscribe(subscription) {
+      validateSubscription(subscription);
 
-  return function componentStateStore(reducer, initialState) {
-    // keep reference to the redux store
-    store = next(componentStateReducer(reducer), initialState);
+      const {
+        key,
+        reducer,
+        initialState,
+        middlewares: componentMiddlewares,
+      } = subscription;
 
-    // return composed store.
-    // The replaceReducer is wrapped to use the component store implementation.
-    // A `componentState` field is added to the main redux store.
-    return {
-      ...store,
-      replaceReducer: (reducerFunc) => store.replaceReducer(componentStateReducer(reducerFunc)),
-      componentState: {
-        subscribe,
+      // compose unique store-key
+      const storeKey = getStateKey(key);
+
+      if (subscribersMap[storeKey]) {
+        throw new Error(`The redux componentStore with key: ${key} is already registered!`);
+      }
+
+      // create redux reducer function
+      subscribersMap[storeKey] = reducer;
+
+      const middlewareAPI = {
+        getState: getState.bind(null, key),
+        dispatch: (action) => dispatch(action),
+      };
+
+      const chain = middlewares.concat(componentMiddlewares || [])
+      .map(middleware => middleware(middlewareAPI));
+
+      const componentStateDispatch = dispatch.bind(null, storeKey);
+      const enhancedDispatch = compose(...chain)(componentStateDispatch);
+
+      // mount the new state on the redux store
+      store.dispatch({
+        type: STATE_ACTION,
+        subType: MOUNT,
+        key: storeKey,
+        state: initialState,
+      });
+
+      // return unsuscriber function
+      return {
+        storeKey,
+        dispatch: enhancedDispatch,
+        unsubscribe: unsubscribe.bind(null, storeKey),
+        getState: getState.bind(null, storeKey),
+      };
+    }
+
+    // ****************************************************************
+    // Reducer wrapper function,
+    // use internally to create a component state reducer
+    // ****************************************************************
+
+    /*
+     * Component State Action Signature:
+     * {
+     *  key: {required} {string},
+     *  type: {required} {string},
+     *  subType: {required} {string},
+     *  state: {optional} {object} pre-composed state to map to the component
+     *          state, used to override initial component state mounting default
+     *          value
+     *  data: {required for subtype === ACTION } original action submitted
+     * }
+     */
+    const reactions = {
+      [MOUNT]: (state, action, reducer) => {
+        state[action.key] = action.state || reducer(undefined, { type: INIT });
+        return state;
       },
+
+      [UNMOUNT]: (state, action) => {
+        delete state[action.key];
+        return state;
+      },
+
+      [ACTION]: (state, action, reducer) => {
+        state[action.key] = reducer(state[action.key], action.data);
+        return state;
+      },
+    };
+
+    function applyComponentStateReducers(action, state, newState) {
+      const { key, type, subType } = action;
+      const reducer = subscribersMap[key];
+
+      const tmpState = Object.keys(subscribersMap).reduce( (acc, storeKey) => {
+        acc[storeKey] = (state || {})[storeKey];
+        return acc;
+      }, {});
+
+      // test if the action received is bound to a component state
+      if (type === STATE_ACTION && reducer) {
+        tmpState[key] = tmpState[key] || {};
+
+        // react properly to component state actions
+        const reaction = reactions[subType] || ( (cs) => cs );
+        reaction(tmpState, action, reducer);
+      }
+      Object.assign(newState, tmpState);
+      return newState;
+    }
+
+    function componentStateReducer(reducer) {
+      // `reducer` is the received original redux store reducer function
+      // return reducer method signature
+      return (state, action) => {
+        // process action with the original reducer
+        let newState = reducer(state, action);
+        newState = applyComponentStateReducers(action, state, newState);
+        return newState;
+      };
+    }
+
+    // ****************************************************************
+    // returned Higher Order Store Creator function
+    // ****************************************************************
+
+    return function componentStateStore(reducer, initialState) {
+      // keep reference to the redux store
+      store = next(componentStateReducer(reducer), initialState);
+
+      // return composed store.
+      // The replaceReducer is wrapped to use the component store implementation.
+      // A `componentState` field is added to the main redux store.
+      return {
+        ...store,
+        replaceReducer: (reducerFunc) => store.replaceReducer(componentStateReducer(reducerFunc)),
+        componentState: {
+          subscribe,
+        },
+      };
     };
   };
 }
